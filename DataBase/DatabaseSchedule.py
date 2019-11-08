2cdd8a72f2483a869ce02203a41c22d2d4b5db84
Ethen Pociask <42627790+epociask@users.noreply.github.com>
2019-11-08 02:29:08 -0800

    Update DatabaseSchedule.py

--- DataBase/DatabaseSchedule.py
+++ DataBase/DatabaseSchedule.py
@@ -1,5 +1,7 @@
 import schedule
 import time
+from PriceDataHandler import *
+from IndicatorDataWriter import *
 
 class DatabaseSchedule:
 
@@ -11,7 +13,7 @@ class DatabaseSchedule:
 
 
 	def start(self):
-		schedule.every(5).seconds.do(self.price_data_hander.connect)
+		schedule.every(5).seconds.do(self.price_data_handler.connect)
 		schedule.every(1).minutes.do(self.indicator_data_handler.connect, ("one-minute"))
 		schedule.every(5).minutes.do(self.indicator_data_handler.connect,("five-minute"))
 		schedule.every(10).minutes.do(self.indicator_data_handler.connect,("ten-minute"))
@@ -22,3 +24,6 @@ class DatabaseSchedule:
 		while 1:
 			schedule.run_pending()
 			time.sleep(1)
+
+
+DatabaseSchedule("ripple").start()
